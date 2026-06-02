79d0c691fc95e49b39ff308e0d31ba6f7766a482
15-10-2009 <tejasvi.jj07@gmail.com>
2026-06-02 09:35:15 +0530

    Skillbridge-backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -126,7 +126,27 @@ exports.getCurrentUser = async (req, res) => {
   }
 };
 
-exports.getProfile = exports.getCurrentUser;
+exports.getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      user: sanitizeUser(user),
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 exports.loginUser = async (req, res) => {
 
